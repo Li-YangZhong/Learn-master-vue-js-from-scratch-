3597710c4a4d05e6c3f7f7ba8057194ed249799f
Eric <liyangzhong@hotmail.com>
2020-03-03 22:07:00 +0800

    using caching to limit api requests

--- 8.21/app.js
+++ 8.21/app.js
@@ -5,9 +5,22 @@ new Vue({
     },
 
     mounted() {
-        axios.get('https://free.currconv.com/api/v7/currencies?apiKey=do-not-use-this-key')
-        .then(response => {
-            this.currencies = response.data.results;
-        })
+        this.getCurrencies();
+    },
+
+    methods: {
+        getCurrencies() {
+            const currencies = localStorage.getItem('currencies')
+
+            if (currencies) {
+                this.currencies = JSON.parse(currencies);
+                return;
+            }
+            axios.get('https://free.currconv.com/api/v7/currencies?apiKey=do-not-use-this-key')
+            .then(response => {
+                this.currencies = response.data.results;
+                localStorage.setItem('currencies', JSON.stringify(response.data.results))
+            })
+        }
     }
 })
